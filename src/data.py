a975d2e7541686993d538cf015005362f1da413e
IanFade <ifade@cs.stonybrook.edu>
2019-12-10 21:00:34 -0500

    Change data loading to support on demand vectorization

--- src/data.py
+++ src/data.py
@@ -3,9 +3,11 @@ import os
 import collections
 import numpy as np
 from tqdm import tqdm
+import random
 
-MAX_COMMENT_LENGTH= 250 #Change this. Ideally 10,000 but taking too much memory
-root_dir = os.path.normpath("/Users/Mallikarjuna/Documents/SBU/Courses/Sem 2/NLP/HW/final_project/DeepVarianceNetwork")
+MAX_COMMENT_LENGTH = 200 #Change this. Ideally 10,000 but taking too much memory
+MIN_COMMENT_LENGTH = 10
+COMMENTS_PER_USER = 100
 
 
 # all_redditors{
@@ -37,9 +39,11 @@ def load_processed_data(data_dir):
 # loads the text comments, processes them, and saves them to the disk
 def load_raw_data(data_file_path):
 
+	maxed_users = set()
 	all_redditors = {}
+	full_redditors = {}
 	
-	with open(data_file_path,'r',newline='') as csv_file:
+	with open(data_file_path,'r',newline='', encoding = 'utf-8') as csv_file:
 		csv_reader = csv.reader(csv_file,delimiter=',',skipinitialspace=True,lineterminator = '\n')
 		
 
@@ -56,6 +60,8 @@ def load_raw_data(data_file_path):
 				continue
 
 			user_id = row[1]
+			while user_id in maxed_users:
+				user_id = user_id + "1"
 
 			if user_id not in all_redditors:
 				all_redditors[user_id] = {
@@ -68,12 +74,18 @@ def load_raw_data(data_file_path):
 					"comment_embeddings":[]
 				}
 			
+			if len(row[3].split(' ')) < MIN_COMMENT_LENGTH:
+				continue
+
 			all_redditors[user_id]['comments'].append({"text":row[3],"timestamp":row[4]})
+			if len(all_redditors[user_id]['comments']) >= COMMENTS_PER_USER:
+				maxed_users.add(user_id)
+				full_redditors[user_id] = all_redditors[user_id]
 
-			if len(all_redditors) == 50:
-				break
+			# if len(all_redditors) == 50:
+			# 	break
 
-	return all_redditors
+	return full_redditors
 
 
 # loads the mappings from words to GloVe embeddings from the disk
@@ -89,7 +101,7 @@ def load_embeddings(embeddings_txt_file: str,embedding_dim: int) -> np.ndarray:
 
     embeddings = {}
     print("\nReading pretrained embedding file.")
-    with open(embeddings_txt_file) as file:
+    with open(embeddings_txt_file, encoding = 'utf-8') as file:
         for line in tqdm(file):
             line = str(line).strip()
             token = line.split(' ', 1)[0]
@@ -121,11 +133,7 @@ def load_embeddings(embeddings_txt_file: str,embedding_dim: int) -> np.ndarray:
 
 # Converts each user comment into its corresponding list of vectors, and corresponding class
 def vectorize_data( all_redditors,all_embeddings):
-
-
 	for user_id in all_redditors:
-
-
 		redditor_obj = all_redditors[user_id]
 		redditor_obj['comment_embeddings'] = np.zeros(shape=(len(redditor_obj['comments']),MAX_COMMENT_LENGTH,50))
 		for ci in range(len( redditor_obj['comments'])):
@@ -135,14 +143,14 @@ def vectorize_data( all_redditors,all_embeddings):
 			embedding_matrix = np.zeros(shape=(MAX_COMMENT_LENGTH,50))
 			for wi in range(MAX_COMMENT_LENGTH):
 				if wi >= len(words_list):
-					word_emb = embeddings['pad_token']
+					word_emb = all_embeddings['pad_token']
 				else:
 					word = words_list[wi].lower()
 					if word != '':
 						if word in all_redditors:
-							word_emb = embeddings[word]
+							word_emb = all_embeddings[word]
 						else:
-							word_emb = embeddings['unk_token']
+							word_emb = all_embeddings['unk_token']
 					else:
 						#print("empty word encountered.Fix this")
 						#TODO Arjun:
@@ -155,6 +163,42 @@ def vectorize_data( all_redditors,all_embeddings):
 	
 	return all_redditors
 
+def alpha_only(word):
+	return ''.join(x for x in word if x in "qwertyuiopasdfghjklzxcvbnm")
+
+def vectorize_user(user, all_embeddings):
+	vectors = np.zeros(shape=(len(user['comments']),MAX_COMMENT_LENGTH,50))
+	for ci in range(len( user['comments'])):
+		comment_obj = user['comments'][ci]
+		comment_text = comment_obj['text']
+		words_list = comment_text.strip().split(' ')
+		embedding_matrix = np.zeros(shape=(MAX_COMMENT_LENGTH,50))
+		for wi in range(MAX_COMMENT_LENGTH):
+			if wi >= len(words_list):
+				word_emb = all_embeddings['pad_token']
+			else:
+				word = words_list[wi].lower()
+				word = alpha_only(word)
+				# if word != '':
+				# 	if word in all_embeddings:
+				# 		word_emb = all_embeddings[word]
+				# 	else:
+				# 		word_emb = all_embeddings['unk_token']
+				# else:
+				# 	#print("empty word encountered.Fix this")
+				# 	#TODO Arjun:
+				# 	continue
+				# 	pass
+				if word in all_embeddings:
+					word_emb = all_embeddings[word]
+				else:
+					word_emb = all_embeddings['unk_token']
+
+			embedding_matrix[wi] = word_emb
+		
+		vectors[ci] = embedding_matrix
+	return vectors
+
 
 # Normalizes the data to have a mean of 0 and a standard deviation of 1
 # Might also want to correct for any class disparity here, else we can do it in the training routine
@@ -162,10 +206,44 @@ def normalize_data(vectorized_data):
 	raise NotImplemented
 
 
-#data_file_path = os.path.join(root_dir, "src/test.csv")
-data_file_path = os.path.join(root_dir, "src/data/reddit_bipolar.csv")
-embeddings_file_path = os.path.join(root_dir,"src/data/glove.6B.50d.txt")
-all_redditors = load_raw_data(data_file_path)
-embeddings = load_embeddings(embeddings_file_path,50)
-all_redditors = vectorize_data(all_redditors,embeddings)
+class DataCorpus(object):
+	def __init__(self):
+		self.positive_samples = []
+		self.negative_samples = []
 
+	def seperate_test(self, ratio):
+		random.shuffle(self.positive_samples)
+		random.shuffle(self.negative_samples)
+		self.positive_test = self.positive_samples[: int(len(self.positive_samples) * ratio)]
+		self.positive_training = self.positive_samples[int(len(self.positive_samples) * ratio) :]
+		self.negative_test = self.negative_samples[: int(len(self.negative_samples) * ratio)]
+		self.negative_training = self.negative_samples[int(len(self.negative_samples) * ratio) :]
+
+	def shuffle(self):
+		random.shuffle(self.positive_test)
+		random.shuffle(self.positive_training)
+		random.shuffle(self.negative_test)
+		random.shuffle(self.negative_training)
+
+	def batch_count(self, batch_size):
+		return int(min(len(self.positive_training), len(self.negative_training)) / batch_size)
+
+	def test_batch_count(self, batch_size):
+		return int(min(len(self.positive_test), len(self.negative_test)) / batch_size)
+
+	def get_batch(self, batch_index, batch_size):
+		batch_start = batch_index * batch_size
+		positive = self.positive_training[batch_start : batch_start + batch_size]
+		negative = self.negative_training[batch_start : batch_start + batch_size]
+
+		return positive + negative
+
+	def get_test(self):
+		return self.positive_test + self.negative_test
+
+	def get_test_batch(self, batch_index, batch_size):
+		batch_start = batch_index * batch_size
+		positive = self.positive_test[batch_start : batch_start + batch_size]
+		negative = self.negative_test[batch_start : batch_start + batch_size]
+
+		return positive + negative
