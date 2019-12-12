Add baseline softmax regression and fix some multiclass errors

src/model.py
@@ -2,20 +2,21 @@ import tensorflow as tf
 from tensorflow.keras import models, layers
 import numpy as np
 
-class BipolarClassifier(models.Model):
+class MentalHealthClassifier(models.Model):
 
 	# Initializes a new copy of the model
-    def __init__(self, embedding_size, layer_sizes, signals = ['mean', 'variance']):
-    	super(BipolarClassifier, self).__init__()
+    def __init__(self, embedding_size, layer_sizes, num_classes, signals = ['mean', 'variance']):
+    	super(MentalHealthClassifier, self).__init__()
     	self.input_size = embedding_size * len(signals)
     	self.dense_layers = []
     	self.signals = signals
+    	self.num_classes = num_classes
     	last_size = self.input_size
     	for layer_size in layer_sizes:
     		self.dense_layers.append(tf.keras.layers.Dense(layer_size, activation = 'tanh', input_shape = (last_size,)))
     		last_size = layer_size
     	# Layer for classification
-    	self.dense_layers.append(tf.keras.layers.Dense(1, activation = 'tanh', input_shape = (last_size,)))
+    	self.dense_layers.append(tf.keras.layers.Dense(num_classes, input_shape = (last_size,)))
 
     # Takes a batch of user comments and classifies each of them as bipolar or not
     # If truth was provided, also calculates the loss
@@ -34,11 +35,46 @@ class BipolarClassifier(models.Model):
     	output_dict['outputs'] = signal
 
     	if truth is not None:
-    		loss = tf.keras.losses.MSE(tf.convert_to_tensor(truth, dtype=tf.float32), signal)
-    		# print(truth)
-    		# print(signal.shape)
-    		# loss = (truth - signal) ** 2
+    		# loss = tf.keras.losses.MSE(tf.convert_to_tensor(truth, dtype=tf.float32), signal)
+    		loss = tf.nn.softmax_cross_entropy_with_logits(
+			    labels = truth,
+			    logits = output_dict['outputs'],
+			)
     		output_dict['loss'] = loss
 
     	# print(output_dict)
     	return output_dict['outputs'], output_dict['loss']
+
+class SoftmaxRegression(models.Model):
+
+    # Initializes a new copy of the model
+    def __init__(self, embedding_size, num_classes, signals = ['mean', 'variance']):
+        super(SoftmaxRegression, self).__init__()
+        self.input_size = embedding_size * len(signals)
+        self.signals = signals
+        self.num_classes = num_classes
+
+        # Layer for classification
+        self.dense_layer = tf.keras.layers.Dense(num_classes, input_shape = (self.input_size,))
+
+    # Takes a batch of user comments and classifies each of them as bipolar or not
+    # If truth was provided, also calculates the loss
+    def call(self, features, truth = None):
+        # signal = np.concatenate([features[x] for x in self.signals], axis = 1)
+        # signal = features['mean']
+        # print(signal.shape)
+        # print(signal)
+        signal = tf.concat([features[x] for x in self.signals], 1)
+
+        output_dict = dict()
+        output_dict[0] = signal
+        output_dict['outputs'] = self.dense_layer(signal)
+
+        if truth is not None:
+            loss = tf.nn.softmax_cross_entropy_with_logits(
+                labels = truth,
+                logits = output_dict['outputs'],
+            )
+            output_dict['loss'] = loss
+
+        return output_dict['outputs'], output_dict['loss']

src/train.py
@@ -7,7 +7,6 @@ import data
 import numpy as np
 
 def average_padded_sequence(seq):
-	# print(seq)
 	count = 0
 	total = None
 	for item in seq:
@@ -25,7 +24,6 @@ def average_padded_sequence(seq):
 
 def variance_padded_sequence(seq):
 	for ind, item in enumerate(seq):
-		# print(item[0])
 		if item[0] == 0:
 			last = max(1, ind - 1)
 			break
@@ -33,7 +31,6 @@ def variance_padded_sequence(seq):
 		last = len(seq)
 
 	unpadded = np.array(seq[:last])
-	# print(unpadded.shape)
 	return np.clip(np.var(unpadded, axis = 0), 0, 10)
 
 def generate_features(users):
@@ -43,115 +40,100 @@ def generate_features(users):
 		for comment in user:
 			comment_averaged[-1].append(average_padded_sequence(comment))
 
-	# print([variance_padded_sequence(x) for x in comment_averaged])
 	features = {
 		'mean' : tf.convert_to_tensor([average_padded_sequence(x) for x in comment_averaged], dtype=tf.float32),
 		'variance' : tf.convert_to_tensor([variance_padded_sequence(x) for x in comment_averaged], dtype=tf.float32)
 	}
-	# print(features['variance'].shape)
-	# print(features['variance'])
 
 	return features
 
+def user_truth(user):
+	truth = np.array([0, 0, 0, 0], dtype = np.float32)
+	if user['on_bipolar'] == '1':
+		truth[1] = 1
+	if user['on_bpd'] == '1':
+		truth[2] = 1
+	if np.sum(truth) == 0:
+		truth[0] = 1
+	elif np.sum(truth) == 2:
+		truth = np.array([0, .1, .1, .8], dtype = np.float32)
+
+	return truth / np.sum(truth)
+
 def test_accuracy(classifier, embeddings, data_corpus):
-	true_positive = 0
-	false_positive = 0
-	true_negative = 0
-	false_negative = 0
+	confusion_matrix = np.zeros([classifier.num_classes, classifier.num_classes], dtype = np.int32)
 	losses = []
-	for batch_ind in range(data_corpus.test_batch_count(32)):
-		batch_users = data_corpus.get_test_batch(batch_ind, 32)
+	batch_size = 128
+	for batch_ind in range(data_corpus.test_batch_count(batch_size)):
+		batch_users = data_corpus.get_test_batch(batch_ind, batch_size)
 		inputs = [data.vectorize_user(x, embeddings) for x in batch_users]
-		truth = [[int(x['on_bipolar']) * 2 - 1] for x in batch_users]
+		truth = [user_truth(x) for x in batch_users]
 		features = generate_features(inputs)
 
 		outputs, loss = classifier(features, truth = truth)
 		losses.append(loss)
 		
 		for t, o in zip(truth, outputs):
-			# print(t,o)
-			if t[0] == 1:
-				if o > 0:
-					true_positive += 1
-				else:
-					false_negative += 1
-			else:
-				if o > 0:
-					false_positive += 1
-				else:
-					true_negative += 1
+			confusion_matrix[np.argmax(t), np.argmax(o)] += 1
 
 	loss = np.sum(losses) / len(losses)
 	print("Test loss: " + str(tf.reduce_mean(loss).numpy()))
-	print(true_positive, true_negative, false_positive, false_negative)
+	print(confusion_matrix)
 	with open('test_loss.txt', 'a') as f:
 		f.write(str(float(tf.reduce_mean(loss).numpy())))
 		f.write('\n')
 	with open('test_acc.txt', 'a') as f:
-		f.write(','.join(str(x) for x in [true_positive, true_negative, false_positive, false_negative]))
+		f.write(str(confusion_matrix))
 		f.write('\n')
 
-def train(classifier, data_corpus, num_epochs, batch_size):
+def train(classifier, data_corpus, num_batches, batch_size):
 	optimizer = tf.keras.optimizers.Adam(learning_rate = 0.0003)
 	embeddings = data.load_embeddings(embeddings_file_path, 50)
-	for epoch in range(num_epochs):
-		data_corpus.shuffle()
-		for batch_ind in range(data_corpus.batch_count(batch_size)):
-			# print(batch_ind, epoch)
-			batch_users = data_corpus.get_batch(batch_ind, batch_size)
+	for batch_ind in range(1, num_batches):
+		# print(batch_ind, epoch)
+		batch_users = data_corpus.get_batch(batch_size)
 
-			# print("About to vectorize")
-			inputs = [data.vectorize_user(x, embeddings) for x in batch_users]
-			truth = [[int(x['on_bipolar']) * 2 - 1] for x in batch_users]
-			features = generate_features(inputs)
-			# print("Features generated")
+		inputs = [data.vectorize_user(x, embeddings) for x in batch_users]
+		truth = [user_truth(x) for x in batch_users]
+		features = generate_features(inputs)
 
-			with tf.GradientTape() as tape:
-				outputs, loss = classifier(features, truth = truth)
-				# print(outputs.numpy(), tf.reduce_mean(loss))
-				print(batch_ind, epoch, tf.reduce_mean(loss).numpy())
-				# print(classifier.trainable_variables)
-				grads = tape.gradient(loss, classifier.trainable_variables)
-				optimizer.apply_gradients(zip(grads, classifier.trainable_variables))
-				with open('train_loss.txt', 'a') as f:
-					f.write(str(float(tf.reduce_mean(loss).numpy())))
-					f.write('\n')
-			if batch_ind % 10 == 0:
-				test_accuracy(classifier, embeddings, data_corpus)
+		with tf.GradientTape() as tape:
+			outputs, loss = classifier(features, truth = truth)
+			print(batch_ind, tf.reduce_mean(loss).numpy())
+			grads = tape.gradient(loss, classifier.trainable_variables)
+			optimizer.apply_gradients(zip(grads, classifier.trainable_variables))
+			with open('train_loss.txt', 'a') as f:
+				f.write(str(float(tf.reduce_mean(loss).numpy())))
+				f.write('\n')
+		if batch_ind % 10 == 0:
+			test_accuracy(classifier, embeddings, data_corpus)
 
 if __name__ == '__main__':
 	root_dir = os.path.normpath(r"C:/repos/DeepVarianceNetwork")
-	# Read arguments or use defaults
 	device_name = tf.test.gpu_device_name()
 	print(f"GPU found: {device_name == '/device:GPU:0'}")
 	print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
-	data_file_path = os.path.join(root_dir, "data/reddit_bipolar.csv")
-	embeddings_file_path = os.path.join(root_dir,"data/glove.6B.50d.txt")
+	data_file_path = os.path.join(root_dir, "data/reddit_data.csv")
 	all_redditors = data.load_raw_data(data_file_path)
-	# embeddings = data.load_embeddings(embeddings_file_path,50)
-	# all_redditors = data.vectorize_data(all_redditors, embeddings)
+	embeddings_file_path = os.path.join(root_dir,"data/glove.6B.50d.txt")
 
 	data_corpus = data.DataCorpus()
 	for user_id in all_redditors:
-		if all_redditors[user_id]['on_bipolar'] == '1':
-			data_corpus.positive_samples.append(all_redditors[user_id])
-		else:
-			data_corpus.negative_samples.append(all_redditors[user_id])
-	print(len(data_corpus.positive_samples))
-	print(len(data_corpus.negative_samples))
+		data_corpus.add_sample(np.argmax(user_truth(all_redditors[user_id])), all_redditors[user_id])
+	for x in data_corpus.classes:
+		print(len(data_corpus.classes[x]))
+
 	data_corpus.seperate_test(0.10)
 
-	# Load existing model if it exists and "Restart" parameter is false
-	# Otherwise initialize a new model
-	classifier = model.BipolarClassifier(embedding_size = 50, layer_sizes = [500, 400, 300, 200, 100], signals = ['mean', 'variance'])
+	classifier = model.MentalHealthClassifier(embedding_size = 50, layer_sizes = [500, 400, 300, 200, 100], signals = ['mean'], num_classes = 4)
+	# classifier = model.SoftmaxRegression(embedding_size = 50, signals = ['mean', 'variance'], num_classes = 4)
+	
 	with open('train_loss.txt', 'w') as f:
 		pass
 	with open('test_loss.txt', 'w') as f:
 		pass
 	with open('test_acc.txt', 'w') as f:
 		pass
-	# Train the models
-	train(classifier, data_corpus, 9999, 32)
 
-	# Save the model
+	train(classifier, data_corpus, 9999, 32)